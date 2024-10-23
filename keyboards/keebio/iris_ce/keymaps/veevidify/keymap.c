87dbe412bade33201712971d29d84059d8fa405d
V N <vee.vidify@gmail.com>
2024-10-24 10:33:09 +1100

    changed my mind

--- keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
@@ -34,13 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌─────────────┬────────┬────────┬─────────────┬─────────────┬─────────────┐                          ┌─────────────┬─────────────┬─────────────┬────────┬────────┬──────────────┐
-     KC_GRV,       KC_1,    KC_2,    KC_3,         KC_4,         KC_5,                                    KC_6,         KC_7,         KC_8,         KC_9,    KC_0,    TO(4),
+     KC_GRV,       KC_1,    KC_2,    KC_3,         KC_4,         KC_5,                                    KC_6,         KC_7,         KC_8,         KC_9,    KC_0,    KC_VOLU,
   //├─────────────┼────────┼────────┼─────────────┼─────────────┼─────────────┤                          ├─────────────┼─────────────┼─────────────┼────────┼────────┼──────────────┤
-     KC_TAB,       KC_Q,    KC_W,    KC_E,         KC_R,         KC_T,                                    KC_Y,         KC_U,         KC_I,         KC_O,    KC_BSLS, KC_VOLU,
+     KC_TAB,       KC_Q,    KC_W,    KC_E,         KC_R,         KC_T,                                    KC_Y,         KC_U,         KC_I,         KC_O,    KC_BSLS, KC_VOLD,
   //├─────────────┼────────┼────────┼─────────────┼─────────────┼─────────────┤                          ├─────────────┼─────────────┼─────────────┼────────┼────────┼──────────────┤
-     KC_LSFT,      KC_A,    KC_S,    LCTL_T(KC_D), LGUI_T(KC_F), LALT_T(KC_G),                            LALT_T(KC_H), LGUI_T(KC_J), LCTL_T(KC_K), KC_L,    KC_P,    KC_VOLD,
+     LT(3,KC_ESC), KC_A,    KC_S,    LCTL_T(KC_D), LGUI_T(KC_F), LALT_T(KC_G),                            LALT_T(KC_H), LGUI_T(KC_J), LCTL_T(KC_K), KC_L,    KC_P,    LT(2,KC_BSPC),
   //├─────────────┼────────┼────────┼─────────────┼─────────────┼─────────────┼────────┐        ┌────────┼─────────────┼─────────────┼─────────────┼────────┼────────┼──────────────┤
-     LT(3,KC_ESC), KC_Z,    KC_X,    KC_C,         KC_V,         KC_B,         KC_ESC,           KC_BSPC, KC_N,         KC_M,         KC_COMM,      KC_DOT,  KC_SLSH, LT(2,KC_BSPC),
+     KC_LSFT,      KC_Z,    KC_X,    KC_C,         KC_V,         KC_B,         KC_ESC,           KC_BSPC, KC_N,         KC_M,         KC_COMM,      KC_DOT,  KC_SLSH, TO(4),
   //└─────────────┴────────┴────────┴─────────────┼────────┬────┴───┬─────────┼────────┘        └────────┼─────────┬───┴────┬────────┼─────────────┴────────┴────────┴──────────────┘
                                                    ALTW,    TL_LOWR, KC_SPC,                              KC_ENT,   KC_RSFT, KC_DEL
                                 //                └────────┴────────┴─────────┘                          └─────────┴────────┴────────┘
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //                    ┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-                         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+                         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, ZOOMI,
   //                    ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-                         ALTW,    _______, _______, _______, KC_DQUO, KC_QUOT,                            KC_QUES, KC_PPLS, KC_LBRC, KC_RBRC, _______, ZOOMI,
+                         ALTW,    _______, _______, _______, KC_DQUO, KC_QUOT,                            KC_QUES, KC_PPLS, KC_LBRC, KC_RBRC, _______, ZOOMO,
   //                    ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-                         SCRSH,   _______, _______, KC_PIPE, KC_MINS, KC_COLN,                            KC_EQL,  KC_SCLN, KC_LCBR, KC_RCBR, _______, ZOOMO,
+                         SCRSH,   _______, _______, KC_PIPE, KC_MINS, KC_COLN,                            KC_EQL,  KC_SCLN, KC_LCBR, KC_RCBR, _______, _______,
   //                    ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
                          _______, _______, _______, _______, KC_UNDS, KC_BSPC, FULLS,            RSNAP,   _______, KC_TILD, KC_LT,   KC_GT,   KC_SLSH, _______,
   //                    └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
