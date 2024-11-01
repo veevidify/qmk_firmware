6c0d453b0ebce5dfa0412006785e33306a0a6126
V N <vee.vidify@gmail.com>
2024-11-01 20:53:13 +1100

    change macro

--- keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
@@ -249,7 +249,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
       return false;
     case JSFUNC: // type () => {\n}<enter>
       if (record->event.pressed) {
-        SEND_STRING("() => {}" SS_TAP(X_LEFT) SS_TAP(X_ENT));
+        SEND_STRING("() => {}" SS_TAP(X_LEFT));
       }
       return false;
 
