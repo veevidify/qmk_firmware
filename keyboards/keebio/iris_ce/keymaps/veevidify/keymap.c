71936c3d6a8dbdfb4e88a9d739ad19d8644d9ff8
V N <vee.vidify@gmail.com>
2024-12-17 14:37:26 +1100

    update combo

--- keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/veevidify/keymap.c
@@ -39,9 +39,9 @@ enum custom_keycodes {
 };
 
 // combo keys
-const uint16_t PROGMEM m_lt_gt[] = {KC_M, KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM lt_gt[] = {KC_COMM, KC_DOT, COMBO_END};
 combo_t key_combos[] = {
-    COMBO(m_lt_gt, KC_ENT),
+    COMBO(lt_gt, KC_ENT),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
